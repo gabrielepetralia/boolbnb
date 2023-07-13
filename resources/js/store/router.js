986810f17494f0b92a291c63cff665204a1a4762
valeriofilareti <valerio.filareti@gmail.com>
2023-07-13 15:13:26 +0200

    import vue router

---
+++ resources/js/store/router.js
@@ -0,0 +1,12 @@
+import {createRouter, createWebHistory} from "vue-router";
+const router = createRouter({
+  history: createWebHistory(),
+  linkExactActiveClass: 'active',
+
+  routes: [
+
+  ]
+})
+
+
+export {router};

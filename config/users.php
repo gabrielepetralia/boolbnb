dcf334165ddd0524aef9eeb4098556b4a549aa27
valeriofilareti <valerio.filareti@gmail.com>
2023-07-13 16:02:06 +0200

    create users seeder

---
+++ config/users.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+  [
+    "name" => "Mario",
+    "lastname" => "Rossi",
+    "email" => "mariorossi@gmail.com",
+    "password" => "12345678",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Ugo",
+    "lastname" => "Deughi",
+    "email" => "ugo@gmail.com",
+    "password" => "123456789",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Pinco",
+    "lastname" => "Pallo",
+    "email" => "pinco@gmail.com",
+    "password" => "1234567891",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Andrea",
+    "lastname" => "Messi",
+    "email" => "messi10@gmail.com",
+    "password" => "12345678912",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Cristiano",
+    "lastname" => "Ronaldo",
+    "email" => "cr7@gmail.com",
+    "password" => "123456789123",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Diego",
+    "lastname" => "Maradona",
+    "email" => "dios@gmail.com",
+    "password" => "1234567891234",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Gianfranco",
+    "lastname" => "Basso",
+    "email" => "gianfry@gmail.com",
+    "password" => "12345678912345",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Gabriele",
+    "lastname" => "Petràlia",
+    "email" => "gp@gmail.com",
+    "password" => "123456789123456",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Pietro",
+    "lastname" => "Muresu",
+    "email" => "pm@gmail.com",
+    "password" => "1234567891234567",
+    "birth_date" => "1996-07-30"
+  ],
+  [
+    "name" => "Andres",
+    "lastname" => "Cayambe",
+    "email" => "ac@gmail.com",
+    "password" => "12345678912345678",
+    "birth_date" => "1996-07-30"
+  ]
+  ];

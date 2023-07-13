2607dfa0c47ec7959c2e15f872c6a2bd35d02a38
gabrielepetralia <gabri.petralia03@gmail.com>
2023-07-13 13:01:53 +0200

    rename sponsorships table

--- database/migrations/2023_07_13_105606_create_sponsorships_table.php
+++ database/migrations/2023_07_13_105606_create_sponsorships_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sponsor_plans', function (Blueprint $table) {
+        Schema::create('sponsorships', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique();
-            $table->decimal('price', 5, 2);
-            $table->integer('duration');
+            $table->string("name", 50)->unique();
+            $table->decimal("price", 5,2);
+            $table->integer("duration");
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sponsor_plans');
+        Schema::dropIfExists('sponsorships');
     }
 };

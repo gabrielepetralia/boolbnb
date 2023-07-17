4e92aede25af08276dd9c08cafe7c6845dcee1c4
gabrielepetralia <gabri.petralia03@gmail.com>
2023-07-17 16:58:19 +0200

    add dates to apartment_sponsorship seeder

--- database/seeders/ApartmentsSponsorshipsTableSeeder.php
+++ database/seeders/ApartmentsSponsorshipsTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Apartment;
 use App\Models\Sponsorship;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ApartmentsSponsorshipsTableSeeder extends Seeder
 {
@@ -14,14 +15,23 @@ class ApartmentsSponsorshipsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
       for ($i = 0; $i < 100; $i++) {
-        $apartment = Apartment::inRandomOrder()->first();
+        $sponsorships = Sponsorship::all();
 
+        $apartment = Apartment::inRandomOrder()->first();
         $sponsorhip_id = Sponsorship::inRandomOrder()->first()->id;
 
-        $apartment->sponsorships()->attach($sponsorhip_id);
+        $duration = Sponsorship::where('id', $sponsorhip_id)->value('duration');
+
+        $start_date = $faker->date() . " " . $faker->time();
+        $end_date = rand(0, 1) ? date('Y-m-d H:i:s', strtotime($start_date . ' +' . $duration . ' hours')) : null;
+
+        $apartment->sponsorships()->attach($sponsorhip_id, [
+          'start_date' => $start_date,
+          'end_date' => $end_date
+        ]);
       }
     }
 }

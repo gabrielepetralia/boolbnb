9ba759d2b5f191d463c6534091b593dba540a06e
gabrielepetralia <gabri.petralia03@gmail.com>
2023-07-18 09:17:34 +0200

    print static cards layout

--- resources/js/components/partials/cards/ApartmentCard.vue
+++ resources/js/components/partials/cards/ApartmentCard.vue
@@ -11,13 +11,16 @@ export default {
 <template>
   <div class="col">
     <a href="#">
-      <div class="apartment-card">
+      <div class="apartment-card mb-4">
 
-        <div class="apartment-img">
-
+        <div class="apartment-img mb-2">
+          <img src="" alt="" onerror=" this.src = '/img/placeholder.webp' ">
         </div>
+
         <div class="apartment-text">
-          <h1>card</h1>
+          <h6 class="mb-1">Villa sul Lago</h6>
+          <span>Via dei Laghi, Como</span>
+          <p><span class="fw-semibold">500 &euro;</span> a notte</p>
         </div>
 
       </div>
@@ -29,6 +32,26 @@ export default {
 @use "../../../../scss/partials/variables" as *;
 
 .apartment-card {
+  color: $dark_gray;
+  .apartment-img {
+    border-radius: 12px;
+    overflow: hidden;
+    aspect-ratio: 1;
 
+    &:hover img {
+      transform: scale(1.05);
+    }
+
+    img {
+      height: 100%;
+      object-position: center;
+      transition: all 0.3s;
+
+    }
+  }
+
+  .apartment-text {
+
+  }
 }
 </style>
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApartmentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,22 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(
+// Route::middleware('auth:sanctum')->group(
 
-  Route::get('/user', function (Request $request) {
-    return $request->user();
-  }),
+//   Route::get('/user', function (Request $request) {
+//     return $request->user();
+//   }),
 
-
-
-);
-
-
-
+// );
 
 Route::namespace('Api')
   ->prefix('apartments')
-  ->group(function(){
+  ->group(function () {
     Route::get('/', [ApartmentController::class, 'getSponsorizedApartments']);
-
 });
+

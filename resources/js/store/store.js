cf4faaafe64086fc3759e55d2b7a2f2e11e92655
Andres Cayambe <andrescayambe99@hotmail.com>
2023-07-20 16:18:18 +0200

    [add]login-register errors

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -8,7 +8,8 @@ export const store = reactive ({
   apiUrl: 'http://127.0.0.1:8000/api/',
 
   user : null,
-  errors: null,
+  errors: {},
+  errorslogin: {},
 
   form : ref({
     name: "",
@@ -23,27 +24,56 @@ export const store = reactive ({
 
 
   handleRegistration(){
+    this.errors = {}
+
+    if (this.form.name.length < 3) {
+      this.errors.name = "Inserisci un nome valido";
+    }
+
+    if (!this.form.email.includes(".")) {
+      this.errors.email = "Inserisci un indirizzo email valido";
+    }
+
+    if (this.form.password.length > 7) {
     axios.post('/register', {
       name: this.form.name,
       email: this.form.email,
       password: this.form.password,
-    }).then(() => {
+    })
+    .catch(error => {
+      this.errors = error.response.data.errors;
+    })
+    .then(response => {
+      console.log(response)
 
       axios.post('/login', {
         email: this.form.email,
         password: this.form.password
       })
+
     }).then(response => {
       this.getUser()
-
     })
+  }else{
+    this.errors.password = "La password deve essere di almeno 8 caratteri"
+  }
+
 
   },
 
   handleLogin(){
+    this.errorslogin = {}
+
+    if (!this.formLogin.loginEmail.includes("@")) {
+      this.errorslogin.email = "Inserisci un indirizzo email valido";
+      console.log(this.errorslogin)
+    }
+    //  if (this.formLogin.loginPassword.length > 7) {
+    //   this.errorslogin.push("La password deve essere di almeno 8 caratteri")
+    //   console.log(this.errorslogin)}
+
 
     if(this.formLogin.loginPassword.length > 7){
-
       axios.get('sanctum/csrf-cookie')
       .then(result =>{
         axios.post('/login', {
@@ -58,12 +88,17 @@ export const store = reactive ({
             loginPassword: ""
           })
         })
-      })
-    }else {
-      this.errors = "La password deve avere almeno 8 caratteri"
-    }
 
-  },
+        .catch(error => {
+          this.errorslogin = error.response.data.errors;
+          console.log('errore',error.response)
+        });
+      })
+
+      }else{
+        this.errorslogin.password = "La password deve essere di almeno 8 caratteri"
+      }
+    },
 
   handleLogout(){
     axios.post('/logout').then(response => {

1c911855af44af48ea17fab42bca14c926032b79
Pietromuresu <muresupietro66@gmail.com>
2023-07-20 20:35:20 +0200

    Get apartments from places

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Helpers\CustomHelper;
+
+
 
 class ApartmentController extends Controller
 {
@@ -26,7 +30,30 @@ class ApartmentController extends Controller
 
   public function getUserApartments($user_id)
   {
+
     $apartments = Apartment::where('user_id', $user_id)->get()->makeHidden('coordinates');
+    $coordinates = DB::table('apartments')
+        ->selectRaw("ST_X(coordinates) as lat, ST_Y(coordinates) as lng")
+        ->where('user_id', $user_id)
+        ->first();
+    return response()->json(compact('apartments', 'coordinates'));
+  }
+
+  public function getApartmentFromPlaces($address, $radiusInMeters)
+  {
+    $coordinates = CustomHelper::getCoordinatesForDistances($address);
+    $apartments = Apartment::select('*')->selectRaw("
+        ST_X(coordinates) AS lat,
+        ST_Y(coordinates) AS lng,
+        ST_DISTANCE(
+            coordinates,
+            POINT($coordinates)
+        ) / 1000 AS distance_in_km
+    ")
+    ->whereRaw("ST_DISTANCE(coordinates, POINT($coordinates)) <= $radiusInMeters")
+    ->get()->makeHidden('coordinates');
+
     return response()->json(compact('apartments'));
   }
+
 }
